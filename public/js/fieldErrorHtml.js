53e0567b80f21d1aa8667d135e30126d6cea65e0
koplakpanda <koplakpanda@gmail.com>
2024-10-15 12:44:06 +0700

    ready to deploy

---
+++ public/js/fieldErrorHtml.js
@@ -0,0 +1,5 @@
+import{u as e}from"./formValidationErrors.js";const o=e(),n=t=>{const r=o.errors[t];return r&&r.length>0?`
+      <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+        ${r.join(", ")}
+      </span>
+    `:""};export{n as f};
