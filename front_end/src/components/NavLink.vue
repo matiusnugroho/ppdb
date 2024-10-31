f5b575071aa62927247ca1caac5b0ffac465a497
koplakpanda <koplakpanda@gmail.com>
2024-10-31 13:31:44 +0700

    change navlink to router-link

--- front_end/src/components/NavLink.vue
+++ front_end/src/components/NavLink.vue
@@ -1,12 +1,13 @@
 <template>
 	<li class="w-full">
-		<a class="md:px-4 py-2 text-sm bg-transparent rounded-lg text-[#666666] hover:text-gray-900 focus:outline-none focus:shadow-outline" :href="url">
+		<router-link class="md:px-4 py-2 text-sm bg-transparent rounded-lg text-[#666666] hover:text-gray-900 focus:outline-none focus:shadow-outline" :to="url">
 			{{ name }}
-		</a>
+		</router-link>
 	</li>
 </template>
 
 <script setup lang="ts">
+
 // Define props using defineProps from Vue
 defineProps<{
 	name: string
