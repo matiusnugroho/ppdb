750dbcff122046bce912a9cf8aa38749ea7b7933
koplakpanda <koplakpanda@gmail.com>
2024-11-01 15:25:17 +0700

    add purgecss

--- front_end/postcss.config.js
+++ front_end/postcss.config.js
@@ -1,6 +1,16 @@
+import purgecss from '@fullhuman/postcss-purgecss'
+
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-  },
+    '@fullhuman/postcss-purgecss': {
+      content: [
+        './src/**/*.vue',
+        './src/**/*.js'
+      ],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      ...purgecss
+    }
+  }
 }
