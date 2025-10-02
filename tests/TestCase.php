65b56bb42ff39978a4c1c521bf0b10116a332d98
Matius Nugroho Aryanto <djmathuse@gmail.com>
2025-10-02 13:59:57 +0700

    chore: upgrade to Laravel 12

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,23 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Create the application instance for testing.
+     */
+    public function createApplication(): Application
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->loadEnvironmentFrom('.env.testing');
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
 }
