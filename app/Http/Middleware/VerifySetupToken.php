6c0dff2a2e7ae1e9bf79a4212b0bb9cf0316585b
matius <matius@vadhana.co.id>
2025-12-12 08:25:31 +0700

    new build

--- app/Http/Middleware/VerifySetupToken.php
+++ app/Http/Middleware/VerifySetupToken.php
@@ -17,7 +17,7 @@ class VerifySetupToken
     public function handle(Request $request, Closure $next): Response
     {
         $token = $request->query('token'); // Token from the request
-        $hashedToken = config('app.route_protection_token'); // Hashed token from the .env file
+        $hashedToken = config('app.route_protection_token');
 
         if (! Hash::check($token, $hashedToken)) {
             abort(403, 'Unauthorized access.');
